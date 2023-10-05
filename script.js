2e9bf8f160e10b14bc2548e66e58712bc7e67a81
Vaisakh <146176692+Vaisakh-2147@users.noreply.github.com>
2023-10-05 19:08:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var i=1;
+function add_to_cart1(){
+  document.getElementById("add_to_carts1").innerHTML="Added to cart";
+  document.getElementById("cart").innerHTML=this.i;
+  this.i=this.i+1;
+}
+function add_to_cart2(){
+  document.getElementById("add_to_carts2").innerHTML="Added to cart";
+  document.getElementById("cart").innerHTML=this.i;
+  this.i=this.i+1;  
+}
+function add_to_cart3(){
+  document.getElementById("add_to_carts3").innerHTML="Added to cart";
+  document.getElementById("cart").innerHTML=this.i;
+  this.i=this.i+1;  
+}
+function add_to_cart4(){
+  document.getElementById("add_to_carts4").innerHTML="Added to cart";
+  document.getElementById("cart").innerHTML=this.i;
+  this.i=this.i+1; 
+}
